000c9b30e99009279180b4fc516bb555a62e0669
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-25 09:15:57 -0500

    ownership in thread with move

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -2,7 +2,34 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    create_new_thread();
+    // create_new_thread();
+    // wait_for_threads_to_finish();
+    ownership_of_vector();
+}
+
+fn ownership_of_vector() {
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+    handle.join().unwrap();
+}
+
+fn wait_for_threads_to_finish() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
 }
 
 
